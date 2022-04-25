5b7d44d00d132a56a90a22215f5c0ed155f1fb3d
"fps" <"anthony.nnaemeka.umeh@gmail.com">
2022-04-25 07:59:48 +0100

    25 April

--- Fake Gta/fakegta.sol
+++ Fake Gta/fakegta.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >0.6.0;
+import "./Libraries/Strings.sol";
 
 /*
  * @title: Fake Gta, just a play around.
@@ -10,6 +11,7 @@ pragma solidity >0.6.0;
 
 contract GTA
 {
+    using Strings for string;
 
     enum State
     {
@@ -22,12 +24,12 @@ contract GTA
         address user_id;
         string username;
         uint8 health;
-        string[] weapons;
+        string[3] weapons;
         string current_weapon;
-        string[] cars;
+        string[3] cars;
         string current_car;
         uint256 value;
-        string[] artifacts;
+        string[3] artifacts;
         string current_artifact;
         State state;
     }
@@ -56,6 +58,53 @@ contract GTA
     * gun = 100 gwei.
     */
 
-    mapping(string => uint32) private costs;
+    mapping(string => uint64) private weapon_costs;
+
+    mapping(string => uint64) private free_weapons;
+
+
+
+    /*
+    * Artifacts mapping. ONLY 3 ARTIFACTS.
+    */
+
+    mapping(string => uint64) private artifact_costs;
+
+
+    /*
+    * Car to cost.
+    * @dev:
+    * Aston Martin = 200 gwei.
+    * Lamborghini = 300 gwei.
+    * Ferrari = 500 gwei.
+    */
+
+    mapping(string => uint64) private car_costs;
+
+
+
+
+    constructor()
+    {
+        damage["hand"] = 1;
+        damage["knife"] = 3;
+        damage["gun"] = 5;
+
+        
+        weapon_costs["hand"] = 0 gwei;
+        weapon_costs["knife"] = 50 gwei;
+        weapon_costs["gun"] = 100 gwei;
+
+
+        artifact_costs["helmet"] = 4 gwei;
+        artifact_costs["boots"] = 7 gwei;
+        artifact_costs["armour"] = 10 gwei;
+
+
+        
+        car_costs["aston martin"] = 200 gwei;
+        car_costs["lamborghini"] = 300 gwei;
+        car_costs["ferrari"] = 500 gwei;
+    }
     
 }
