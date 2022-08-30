4d3a0d1c81214ec81f781301ee62b56a011feb91
fps <anthony.nnaemeka.umeh@gmail.com>
2022-08-30 15:32:56 +0100

    Update TryCatch.sol

--- TryCatch.sol
+++ TryCatch.sol
@@ -7,6 +7,8 @@ pragma solidity ^0.8.14;
 * @dev Lets try to catch.
 */
 contract TryCatch {
+    error Error(string memory reason);
+    
     try IERC1155Receiver(to).onERC1155Received(operator, from, id, amount, data) returns (bytes4 response) {
         if (response != IERC1155Receiver.onERC1155Received.selector) {
             revert("ERC1155: ERC1155Receiver rejected tokens");
